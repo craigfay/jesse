4ac7892cae2fda98d05e3933d7d5ea06e510abd4
craig fay <craigfay1@gmail.com>
2020-07-28 09:26:24 -0500

    Log addr

--- src/bin/alt_server.rs
+++ src/bin/alt_server.rs
@@ -22,16 +22,22 @@ async fn read_post_handler(params: web::Path<Params>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let addr = "127.0.0.1:8000";
+    println!("Listening at {}", addr);
+
     HttpServer::new(|| {
         App::new().service(
+
             web::scope("/api")
-                .route("/posts/{id}.json", web::get().to(read_post_handler))
+                .route("/posts/{id}.json",
+                    web::get().to(read_post_handler))
         )
 
     })
-    .bind("127.0.0.1:8000")?
+    .bind(&addr)?
     .run()
     .await
+
 }
 
 
