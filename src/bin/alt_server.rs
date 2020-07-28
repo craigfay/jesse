09b8297b9f4a38adfe40060e20371320ad26dd42
craig fay <craigfay1@gmail.com>
2020-07-28 09:22:16 -0500

    Introduce actix alt server

---
+++ src/bin/alt_server.rs
@@ -0,0 +1,39 @@
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+extern crate rust_json_api;
+
+
+use rust_json_api::db;
+use serde_json;
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Deserialize)]
+struct Params {
+    id: i32,
+}
+
+
+async fn read_post_handler(params: web::Path<Params>) -> impl Responder {
+    let id = params.id;
+    format!("Hello {}!", id)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/api")
+                .route("/posts/{id}.json", web::get().to(read_post_handler))
+        )
+
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+
+
+
